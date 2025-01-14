f1659cfaf8abf03e2a309d17688a1612e0a23cfd
aitor <aitor.mendiburu@ikasle.aeg.eus>
2025-01-14 10:20:47 +0100

    fix data from ingredients and maximum 2 to 4 ingredients

--- index.mjs
+++ index.mjs
@@ -3,9 +3,25 @@ import { getDiseases, getIngredients, getPlayerData } from "./api.mjs";
 import PotionIngredients from "./ingredients.mjs";
 
 
-function potionCreation (ingredientsAPI, diseases, playerData) {
+function randomSelectedIngredients (ingredientsAPI) {
+    let randomIngredients = [];
+    let randomIngredient = {};
+    let randomQuantity = Math.floor(Math.random() * (4 - 2 + 1) + 2);
 
-    const ingredients = PotionIngredients.load(ingredientsAPI);
+    for (let i = 0; i < randomQuantity; i++) {
+        randomIngredient = ingredientsAPI.at(Math.floor(Math.random() * (ingredientsAPI.length - 0 + 1) + 0))
+        randomIngredients.push(randomIngredient);
+    }
+
+    return randomIngredients
+}
+
+
+function potionCreation (randomSelectedIngredients, diseases, playerData) {
+
+    console.log("creating potions");
+
+    const ingredients = PotionIngredients.load(randomSelectedIngredients);
 
     // Create the cauldron with the ingredient instances
     const cauldron = new Cauldron(ingredients, diseases, playerData);
@@ -17,13 +33,15 @@ function potionCreation (ingredientsAPI, diseases, playerData) {
 
 }
 
-const ingredients = await getDiseases();
-const diseases = await getIngredients();
+const diseases = await getDiseases();
+const ingredientsAll = await getIngredients();
 const playerData = await getPlayerData("aitor.mendiburu@ikasle.aeg.eus");
 
+let selectIngredient = randomSelectedIngredients(ingredientsAll.data);
+console.log(selectIngredient);
 
-////potionCreation(ingredients, diseases, playerData)
 
-console.log("hola");
+potionCreation(selectIngredient, diseases, playerData);
+
 
 
